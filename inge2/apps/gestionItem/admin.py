7a221f90ac5a43989d9845d924fbe6005009937e
Gilberto-Martinez <57020462+Gilberto-Martinez@users.noreply.github.com>
2019-11-15 17:27:47 -0300

    Modificacion en tabla LineaBase y en tabla Item

--- inge2/apps/gestionItem/admin.py
+++ inge2/apps/gestionItem/admin.py
@@ -5,21 +5,24 @@ from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
+class AdminLineaBase(admin.ModelAdmin):
+    list_display = ["id_linea_base", "enunciado", "tarea", "duenho_voluntario", "estado",
+					"horas_estimadas", "id_proyecto"]
+    list_filter = ["id_linea_base"]
+    list_editable = ["enunciado", "tarea", "duenho_voluntario", "estado",
+					"horas_estimadas", "id_proyecto"]
+    search_fields = ["id_linea_base"]
+
 class AdminProyecto(admin.ModelAdmin):
     list_display = ["id_proyecto", "nombre", "fase", "fecha_inicio", "fecha_fin"]
     list_filter = ["nombre"]
     list_editable = ["nombre", "fase", "fecha_inicio", "fecha_fin"]
     search_fields = ["nombre"]
 
-"""class AdminUsuario(admin.ModelAdmin):
-    list_display = ["cedula_identidad", "nombre", "apellido", "estado", "rol"]
-    list_filter = ["nombre"]
-    list_editable = ["nombre", "apellido", "estado", "rol"]
-    search_fields = ["nombre"]"""
-
 class PersonalizadoUserAdmin(UserAdmin):
 	fieldsets = ()
 	add_fields = ((None, {
+		'classes': ('wide',),
 		'fields': ('usuario', 'password1', 'password2')
 		}))
 	list_display = ["usuario", "nombre", "apellido", "estado", "rol"]
@@ -27,7 +30,13 @@ class PersonalizadoUserAdmin(UserAdmin):
 	search_fields = ["usuario"]
 	ordering = ["usuario"]
 
+class AdminItem(admin.ModelAdmin):
+    list_display = ["id_item", "descripcion", "estado", "id_linea_base", "id_usuario_creador",]
+    list_filter = ["id_item"]
+    list_editable = ["descripcion", "estado", "id_linea_base", "id_usuario_creador",]
+    search_fields = ["id_item"]
+
 admin.site.register(Proyecto, AdminProyecto)
-admin.site.register(LineaBase)
+admin.site.register(LineaBase, AdminLineaBase)
 admin.site.register(Usuario, PersonalizadoUserAdmin)
-admin.site.register(Item)
+admin.site.register(Item, AdminItem)
--- inge2/apps/gestionItem/models.py
+++ inge2/apps/gestionItem/models.py
@@ -19,10 +19,10 @@ class Proyecto(models.Model):
 
 class LineaBase(models.Model):
     id_linea_base = models.CharField(max_length=10)
-    estado = models.CharField(max_length=10)
     enunciado = models.CharField(max_length=400)
-    tarea = models.CharField(max_length=50)
+    tarea = models.CharField(max_length=70)
     duenho_voluntario = models.CharField(max_length=60)
+    estado = models.CharField(max_length=15)
     horas_estimadas = models.PositiveSmallIntegerField()
     id_proyecto = models.ForeignKey(Proyecto, null=False, blank=False, on_delete=models.CASCADE)
 
@@ -38,7 +38,7 @@ class PersonalizadoBaseUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, usuario, password):
-        user = self.create_user(usuario, password)
+        user = self.create_user(self, usuario, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -46,12 +46,11 @@ class PersonalizadoBaseUserManager(BaseUserManager):
 
 
 class Usuario(AbstractBaseUser, PermissionsMixin):
-    cedula_identidad = models.CharField(max_length=10)
-    usuario = models.CharField(max_length=40, unique=True, default=' ') 
+    usuario = models.CharField(max_length=40, unique=True, default='invitado')
+    cedula_identidad = models.CharField(max_length=10) 
     nombre = models.CharField(max_length=40)
-    apellido = models.CharField(max_length=40)
     correo = models.CharField(max_length=50)
-    contrasenha = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=40)
     id_proyecto = models.ForeignKey(Proyecto, null=True, blank=False, on_delete=models.CASCADE)
     estados = (('A', 'Activo'), ('I', 'Inacivo'))
     estado = models.CharField(max_length=8, choices=estados)
@@ -76,16 +75,16 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
 
 
 class Item(models.Model):
-    id_item = models.CharField(max_length=10)
+    id_item = models.CharField(max_length=15)
     descripcion = models.CharField(max_length=400)
-    prioridad = models.CharField(max_length=10)
-    estado = models.CharField(max_length=10)
+    prioridades = (('A', 'Alta'), ('M', 'Media'), ('B', 'Baja'))
+    prioridad = models.CharField(max_length=15, choices=prioridades)
+    estado = models.CharField(max_length=15)
     atributo = models.CharField(max_length=20)
     version = models.CharField(max_length=10)
-    observacion = models.CharField(max_length=400)
+    observacion = models.CharField(max_length=400, blank= True)
     id_linea_base = models.ForeignKey(LineaBase, null=False, blank=False, on_delete=models.CASCADE)
     id_usuario_creador = models.ForeignKey(Usuario, null=False, blank=False, on_delete=models.CASCADE)
-    # archivo_externo
 
     def __str__(self):
         return self.id_item
